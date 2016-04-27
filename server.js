e470f856e3bdb85ee248ae1ad9c00178131c1e30
Kyle <kyleludlow@gmail.com>
2016-04-27 15:36:38 -0700

    has full functionality

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-var Storage = function() { // constructor
+var Storage = function () {
     this.items = [];
     this.id = 0;
 };
 
-Storage.prototype.add = function(name) { // created a method .add
+Storage.prototype.add = function (name) {
     var item = {name: name, id: this.id};
     this.items.push(item);
     this.id += 1;
     return item;
 };
-// create prototype that deletes items from array
-// this.items.splice(index of item that we wish to delete(must be dynamic), how many items we want deleted from array)
 
 
-Storage.prototype.delete = function (id){ //created a delete method
-    for (var arrayIndex in this.items) { //created a for in loop that loops through the this.items array
+Storage.prototype.delete = function (id) {
+    for (var arrayIndex in this.items) {
 
-        if (id === this.items[arrayIndex].id){
+        if (id === this.items[arrayIndex].id) {
             this.items.splice(arrayIndex, 1);
             return true;
         }
     }
 };
-Storage.prototype.modify = function(newObject){ //
-        for (var arrayIndex in this.items) {
-            if (newObject.id === this.items[arrayIndex].id){
-                this.items[arrayIndex] = newObject;
-                return true;
-            }
+Storage.prototype.modify = function (newObject) {
+    for (var arrayIndex in this.items) {
+        if (newObject.id === this.items[arrayIndex].id) {
+            this.items[arrayIndex] = newObject;
+            return true;
         }
+    }
 };
 
 var storage = new Storage();
@@ -40,17 +38,16 @@ storage.add('Tomatoes');
 storage.add('Peppers');
 storage.add('Cologne');
 
-storage.delete(1);
-console.log(storage);
 
 var app = express();
+
 app.use(express.static('public'));
 
-app.get('/items', function(req, res) {
+app.get('/items', function (req, res) {
     res.json(storage.items);
 });
 
-app.post('/items', jsonParser, function(req, res) {
+app.post('/items', jsonParser, function (req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
@@ -59,7 +56,7 @@ app.post('/items', jsonParser, function(req, res) {
     res.status(201).json(item);
 });
 
-app.delete('/items/:id', jsonParser, function(req, res) {
+app.delete('/items/:id', jsonParser, function (req, res) {
 
     var itemId = parseInt(req.params.id, 10);
 
@@ -70,17 +67,13 @@ app.delete('/items/:id', jsonParser, function(req, res) {
     }
 });
 
-app.put('/items/:id', jsonParser, function(req, res) {
-    // var itemId = parseInt(req.params.id, 10);
-    if (!storage.modify(req.body)){
+app.put('/items/:id', jsonParser, function (req, res) {
+    if (!storage.modify(req.body)) {
         return res.sendStatus(400);
     } else {
         res.status(201).json(storage.items);
     }
 
-    console.log(storage.items);
-
 });
-
 
 app.listen(process.env.PORT || 8080);
